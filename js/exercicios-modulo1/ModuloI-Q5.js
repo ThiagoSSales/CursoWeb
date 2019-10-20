5531d4ce172c27309053f36a22e1da215db0f4a8
Thiago Sales <thiago.ss133@gmail.com>
2019-10-20 19:22:02 -0300

    Melhorando questao 5.

--- js/exercicios-modulo1/ModuloI-Q5.js
+++ js/exercicios-modulo1/ModuloI-Q5.js
@@ -1,12 +1,11 @@
 const aluno = {
     nome: 'iago',
     notas: [
-        {nome: 'fisica', valor: 5},
-        {nome: 'estatistica', valor: 4},
-        {nome: 'probabilidade', valor: 1}
+        { nome: 'fisica', valor: 5 },
+        { nome: 'estatistica', valor: 4 },
+        { nome: 'probabilidade', valor: 1 }
     ]
 }
 
-const getNota = nota => nota.valor
-
-console.log(aluno.notas.map(getNota))
+const getNota = nota => (' ' + nota.nome + ': ' + nota.valor)
+console.log('Notas: \n' + aluno.notas.map(getNota))
